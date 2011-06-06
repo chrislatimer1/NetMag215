aa9fa6f581c749804a47921326b28d2e3a6e8b6d
Chris Latimer <chrislatimer1@gmail.com>
2011-06-06 23:12:43 +0100

    added a title to the output

--- stats.rb
+++ stats.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+puts "Ruby Stats Script"
 words = File.read('stopwords.txt').split(',')
 puts "Number of words: " + words.size.to_s
 puts "Average word size: " + words.size.to_s
